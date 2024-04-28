56bbb18ec79bc520abbb092467683b5ca7a370f8
Adrian <adrian.hamzah15@gmail.com>
2024-04-28 20:08:19 +0700

    feat: add user playlist

--- playlist/urls.py
+++ playlist/urls.py
@@ -3,6 +3,6 @@ from playlist.views import *
 
 
 urlpatterns = [
-    path('', show_akun, name='show_akun'),
+    path('show_playlist_by_username/<str:email>', show_playlist_by_email, name='show_akun'),
     path('show_song', show_song, name='show_song')
 ]
--- playlist/views.py
+++ playlist/views.py
@@ -5,11 +5,14 @@ from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
 
 
-def show_akun(request):
-    query_str = "SELECT * FROM akun"
-    hasil = query(query_str)
-    print(hasil)
-    return render(request, 'index.html', {'akun': hasil})
+def show_playlist_by_email(request, email):
+    print(email)
+    query_str = f"SELECT * FROM USER_PLAYLIST WHERE email_pembuat='{email}'"
+    user_playlists = query(query_str)
+    # print(user_playlists)
+    for user_playlist in user_playlists:
+        user_playlist['tanggal_dibuat'] = user_playlist['tanggal_dibuat'].strftime('%Y-%m-%d')
+    return render(request, 'index.html', {'akun': user_playlists})
 
 # Create your views here.
 def show_song(request):
