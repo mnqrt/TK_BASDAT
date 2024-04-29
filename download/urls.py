350fa5869d4a49e354a9a93c4b67fa2fa70fe00b
Yosef <yosefnuragaw@gmail.com>
2024-04-29 13:43:54 +0700

    feat:show download by email

--- download/urls.py
+++ download/urls.py
@@ -4,4 +4,5 @@ from download.views import *
 app_name = 'download'
 urlpatterns = [
     path('', show_download, name='show_download'),
+    path('by/<str:email>/', show_download_by_email, name='by_email'),  
 ]
--- download/views.py
+++ download/views.py
@@ -2,8 +2,24 @@ from django.shortcuts import render
 from utils.query import query
 
 def show_download(request):
-    query_str = f"SELECT AK.nama, K.judul, ds.email_downloader FROM downloaded_song DS LEFT JOIN song S ON DS.id_song = S.id_konten LEFT JOIN artis A ON S.id_artist = A.id LEFT JOIN akun AK ON A.email_akun = AK.email LEFT JOIN konten K ON DS.id_song = K.id"
-    # query_str = f"SELECT * FROM downloaded_song DS"
+    query_str = f"""SELECT AK.nama, K.judul, ds.email_downloader
+                    FROM downloaded_song DS
+                    LEFT JOIN song S ON DS.id_song = S.id_konten
+                    LEFT JOIN artis A ON S.id_artist = A.id
+                    LEFT JOIN akun AK ON A.email_akun = AK.email
+                    LEFT JOIN konten K ON DS.id_song = K.id"""
+    hasil = query(query_str)
+    print(hasil)
+    return render(request, 'download/index.html', {'konten': hasil})
+
+def show_download_by_email(request,email):
+    query_str = f"""SELECT AK.nama, K.judul, ds.email_downloader
+                    FROM downloaded_song DS
+                    LEFT JOIN song S ON DS.id_song = S.id_konten
+                    LEFT JOIN artis A ON S.id_artist = A.id
+                    LEFT JOIN akun AK ON A.email_akun = AK.email
+                    LEFT JOIN konten K ON DS.id_song = K.id
+                    WHERE DS.email_downloader= '{email}'"""
     hasil = query(query_str)
     print(hasil)
     return render(request, 'download/index.html', {'konten': hasil})
