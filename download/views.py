b118f7ecd3b0e17f8129ba7dff1e27c7481c5c78
Yosef <yosefnuragaw@gmail.com>
2024-05-01 01:01:21 +0700

    feat: navbar and login

--- download/views.py
+++ download/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from utils.query import query
-
+from utils.session_data import get_session_data
 def show_download(request):
+    context = get_session_data(request)
     query_str = f"""SELECT AK.nama, K.judul, ds.email_downloader
                     FROM downloaded_song DS
                     LEFT JOIN song S ON DS.id_song = S.id_konten
@@ -9,8 +10,7 @@ def show_download(request):
                     LEFT JOIN akun AK ON A.email_akun = AK.email
                     LEFT JOIN konten K ON DS.id_song = K.id"""
     hasil = query(query_str)
-    print(hasil)
-    return render(request, 'download/index.html', {'konten': hasil})
+    return render(request, 'download/index.html', {'konten': hasil,'context':context})
 
 def show_download_by_email(request,email):
     query_str = f"""SELECT AK.nama, K.judul, ds.email_downloader
@@ -21,5 +21,4 @@ def show_download_by_email(request,email):
                     LEFT JOIN konten K ON DS.id_song = K.id
                     WHERE DS.email_downloader= '{email}'"""
     hasil = query(query_str)
-    print(hasil)
     return render(request, 'download/index.html', {'konten': hasil})
