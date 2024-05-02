e53ef0093254d08ee77ff6317cd3499e209478da
Adrial Natanael Liong <adrial2_nathanael@yahoo.com>
2024-05-02 13:31:32 +0700

    selesai html cek royalti, kelola album

--- kelola_album_artis/models.py
+++ kelola_album_artis/models.py
@@ -1,34 +1,3 @@
-import uuid
 from django.db import models
 
-class Genre(models.Model):
-    id_konten = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
-    genre = models.CharField(max_length=50)
-
-    class Meta:
-        unique_together = ('id_konten', 'genre')
-
-class Lagu(models.Model):
-    id_konten = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
-    id_artist = models.ForeignKey('Artist', on_delete=models.CASCADE, related_name='songs')
-    id_album = models.ForeignKey('Album', on_delete=models.CASCADE, related_name='songs')
-    total_play = models.IntegerField(default=0)
-    total_download = models.IntegerField(default=0)
-    genres = models.ManyToManyField(Genre, related_name='songs')
-
-class Album(models.Model):
-    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
-    judul = models.CharField(max_length=100)
-    jumlah_lagu = models.IntegerField(default=0)
-    id_label = models.ForeignKey('Label', on_delete=models.CASCADE, related_name='albums')
-    total_durasi = models.IntegerField(default=0)
-
-class Artist(models.Model):
-    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
-    email_akun = models.CharField(max_length=50)
-    id_pemilik_hak_cipta = models.ForeignKey('PemilikHakCipta', on_delete=models.CASCADE, related_name='artists')
-
-class Songwriter(models.Model):
-    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
-    email_akun = models.CharField(max_length=50)
-    id_pemilik_hak_cipta = models.ForeignKey('PemilikHakCipta', on_delete=models.CASCADE, related_name='songwriters')
+# Create your models here.
